29f8f2af517cc2375d6847616ebc41cf504e3609
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2020-05-14 07:19:47 +0700

    change code for ajt project

--- assets/js/index-lama.js
+++ assets/js/index-lama.js
@@ -39,11 +39,12 @@ function onConnectionLost(responseObject) {
 
 // called when a message arrives
 function onMessageArrived(message) {
+  console.log(message)
   let topic = message.destinationName;
   let value = message.payloadString;
   topic = topic.split("/");
-  console.log(topic)
-  if (topic[1] == "temperature") {
+  if (topic[3] == "temperature") {
+    $("#time").addClass("blink");
     $("#temperature").html(value);
     $("#temperature")
       .parent()
@@ -53,29 +54,41 @@ function onMessageArrived(message) {
         .parent()
         .removeClass("blink");
     }, 500);
-  } else if (topic[1] == "humidity") {
+  } else if (topic[3] == "humidity") {
     $("#humidity").html(value);
     $("#humidity")
       .parent()
       .addClass("blink");
     setTimeout(function() {
+      $("#time").removeClass("blink");
       $("#humidity")
         .parent()
         .removeClass("blink");
     }, 500);
-  } else if (topic[1] == "battery") {
-    $("#battery").html(value);
-    $("#battery")
-      .parent()
-      .addClass("blink");
-    setTimeout(function() {
-      $("#battery")
-        .parent()
-        .removeClass("blink");
-    }, 500);
+  } else if (topic[3] == "ledStatus") {
+    if (value == "ON") {
+      $("#led").bootstrapToggle("on", true);
+    } else if (value == "OFF") {
+      $("#led").bootstrapToggle("off", true);
+    }
   }
   firstMessage = true;
-  console.log(`${topic[1]} : ${value}`);
+  var currentdate = new Date();
+  var datetime = `${currentdate.getDate()}/${currentdate.getMonth() +
+    1}/${currentdate.getFullYear()}, ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`;
+  console.log(datetime);
+  $("#time").html(datetime);
+  console.log(`${topic[3]} : ${value}`);
+}
+
+$("#led").change(function() {
+  message = new Paho.MQTT.Message(checkLEDStatus() ? "1" : "0");
+  message.destinationName = "/fakhri19/esp32/led";
+  client.send(message);
+});
+
+function checkLEDStatus() {
+  return document.getElementById("led").checked;
 }
 
 let firstMessage = false;
@@ -88,8 +101,9 @@ function isSensorActive() {
         title: "Sorry",
         text: "The sensor is currently offline"
       });
+      console.log("kosong");
     } else {
       $("#led").bootstrapToggle("enable");
     }
-  }, 3000);
+  }, 2500);
 }
